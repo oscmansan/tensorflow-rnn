4fe556dd3fb9196fe3759ce36bf32d4c03d77ff5
Oscar Mañas <oscmansan@gmail.com>
2019-05-20 20:45:00 +0200

    Refactor

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
+import argparse
+
 import tensorflow as tf
 import tensorflow_datasets as tfds
 from tensorflow.python import keras
 
 
-gpus = tf.config.experimental.list_physical_devices('GPU')
-tf.config.experimental.set_memory_growth(gpus[0], True)
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batch-size', type=int, default=256)
+    parser.add_argument('--epochs', type=int, default=300)
+    parser.add_argument('--lr', type=float, default=0.01)
+    return parser.parse_args()
 
 
 def preprocess(x, y):
@@ -12,24 +18,35 @@ def preprocess(x, y):
     return x, y
 
 
-data, info = tfds.load('mnist', data_dir='data', with_info=True, as_supervised=True)
-print(info)
+def main():
+    args = parse_args()
+    print(vars(args))
 
-train_dataset = data['train'].map(preprocess).batch(256)
-test_dataset = data['test'].map(preprocess).batch(256)
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    tf.config.experimental.set_memory_growth(gpus[0], True)
 
-model = keras.Sequential([
-    keras.layers.Reshape((28, 28), input_shape=(28, 28, 1)),
-    keras.layers.CuDNNLSTM(128),
-    keras.layers.Dense(10, activation='softmax')
-])
-model.summary()
+    data, info = tfds.load('mnist', data_dir='data', with_info=True, as_supervised=True)
+    print(info)
 
-opt = keras.optimizers.SGD(lr=0.001)
-model.compile(loss='sparse_categorical_crossentropy',
-              optimizer=opt,
-              metrics=['accuracy'])
+    train_dataset = data['train'].map(preprocess).batch(args.batch_size)
+    test_dataset = data['test'].map(preprocess).batch(args.batch_size)
 
-history = model.fit(train_dataset,
-                    epochs=300,
-                    validation_data=test_dataset)
+    model = keras.Sequential([
+        keras.layers.Reshape((28, 28), input_shape=(28, 28, 1)),
+        keras.layers.CuDNNLSTM(128),
+        keras.layers.Dense(10, activation='softmax')
+    ])
+    model.summary()
+
+    opt = keras.optimizers.SGD(lr=args.lr)
+    model.compile(loss='sparse_categorical_crossentropy',
+                  optimizer=opt,
+                  metrics=['accuracy'])
+
+    model.fit(train_dataset,
+              epochs=args.epochs,
+              validation_data=test_dataset)
+
+
+if __name__ == '__main__':
+    main()
